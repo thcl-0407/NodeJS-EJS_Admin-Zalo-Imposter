182a90261bef97b88635123e6ca2386bf1d15047
thcl-0407 <thcloc@icloud.com>
2020-10-19 11:25:32 +0700

    update ui home page

--- admin.js
+++ admin.js
@@ -4,11 +4,12 @@ const session = require('express-session')
 const bodyParser = require('body-parser')
 const passport = require('./passport.js')
 const app = express();
-const Port = 3000;
+const Port = 3001;
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(session(
     {
+        name: 'session_login',
         secret: 'cookie_secret',
         resave: false,
         saveUninitialized: false,
@@ -51,9 +52,17 @@ app.get('/home', (req, res)=>{
     }
 })
 
+app.get('/home/usermanage', (req, res)=>{
+    if(req.isAuthenticated()){
+        res.render('usermanage')
+    }else{
+        res.redirect('/login')
+    }
+})
+
 app.post('/logout', (req, res)=>{
     req.logOut()
-    res.clearCookie('connect.sid')
+    res.clearCookie('session_login')
     res.redirect('/')
 })
 
