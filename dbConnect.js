a01538b4c1bc8bab39de929b3e6f7a1a8ced093f
Tran Huynh Cong Loc <71317398+thcl-0407@users.noreply.github.com>
2020-12-18 10:53:31 +0700

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -3,7 +3,7 @@ const sql = require('mssql')
 const pool = new sql.ConnectionPool({
     user: 'admin',
     password: 'sapassword',
-    server: 'aws-sql-server-db.c7i6un41glyu.ap-southeast-1.rds.amazonaws.com',
+    server: 'importes.cdi9bnuvfhs5.ap-southeast-1.rds.amazonaws.com',
     database: 'IMPOSTER',
 });
 
